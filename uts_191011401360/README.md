9961b3ac548d424699a850f897f623da0c6db707
Rosyd Aqbar <idaqbar@gmail.com>
2022-03-26 16:15:59 +0700

    Submit pertemuan 7

--- uts_191011401360/README.md
+++
@@ -1,7 +0,0 @@
-# uts_191011401360
-
-UTS Mobile Programming
-
-Nama : Raden Rosyd Aqbar Setiyanto
-NIM : 191011401360
-Kelas : 06TPLE019
--- uts_191011401360/android/app/src/main/kotlin/com/example/uts_191011401360/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.uts_191011401360
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
